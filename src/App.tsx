93c91903f3e4a4a50c94193f7c9b631bf0f3c3f7
Mark Henson Bautista <mark.hensonbautista@gmail.com>
2023-07-18 13:42:26 +0900

    Updated gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
-import './App.css';
-import Portfolio from './page/Portfolio';
-import ProjectDetails from './page/ProjectDetails';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Portfolio from "./page/Portfolio";
+import ProjectDetails from "./page/ProjectDetails";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={
-          <div className="app-container">  {/* style={{backgroundColor: 'rgb(33, 42, 62)'}}>*/}
-            <Portfolio/>
-          </div>} 
+        <Route
+          path="/portfolio"
+          element={
+            <div className="app-container">
+              {" "}
+              {/* style={{backgroundColor: 'rgb(33, 42, 62)'}}>*/}
+              <Portfolio />
+            </div>
+          }
         />
-        <Route path="/project/:id" element={
-          <div style={{backgroundColor: 'rgb(33, 42, 62)'}}>
-            <ProjectDetails />
-          </div>
-          } 
+        <Route
+          path="/project/:id"
+          element={
+            <div style={{ backgroundColor: "rgb(33, 42, 62)" }}>
+              <ProjectDetails />
+            </div>
+          }
         />
       </Routes>
     </Router>
